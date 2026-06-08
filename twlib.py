ec8236fa9e0363a496fd51565f8018227c744c05
me <me@localhost>
2026-06-08 13:16:40 +0300

    implement @runtime decorator

---
+++ twlib.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+import functools
+import time
+
+def runtime(func):
+    """print run time of function"""
+
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_seconds = end_time - start_time
+        formatted_time = str(timedelta(seconds=int(elapsed_seconds)))
+        print(f"{func.__name__} run time: {formatted_time}")
+        
+    return wrapper_decorator
+
+@runtime
+def long_function():
+    time.sleep(3)
+
+if __name__ == "__main__":
+    long_function()
+
