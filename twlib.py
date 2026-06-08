4359109c2446466a0557b6d232f6ddbf7dbe7caa
me <me@localhost>
2026-06-09 00:21:05 +0300

    fix decorator to work with functions that return something

--- twlib.py
+++ twlib.py
@@ -8,12 +8,13 @@ def runtime(func):
     @functools.wraps(func)
     def wrapper_decorator(*args, **kwargs):
         start_time = time.perf_counter()
-        func(*args, **kwargs)
+        result = func(*args, **kwargs)
         end_time = time.perf_counter()
         elapsed_seconds = end_time - start_time
         formatted_time = str(timedelta(seconds=int(elapsed_seconds)))
         print(f"{func.__name__} run time: {formatted_time}")
-        
+        return result
+
     return wrapper_decorator
 
 @runtime
